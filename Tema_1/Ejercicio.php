5c3dd1cc56589a6074e9e8bb37e2f6ae2d592f43
Jsr-17 <jarmoreno5@gmail.com>
2024-09-24 09:05:37 +0200

    ejercicios clase 23 septiembre

---
+++ Tema_1/Ejercicio.php
@@ -0,0 +1,28 @@
+<?php
+    $name="Jose Antonio";
+    $edad=20;
+    $ciudad="Granada";
+    printf(" Mi nombre es: %s  tengo %d anyos y soy de %s", $name, $edad, $ciudad);
+
+    $n1=12;
+    $n2=3;
+
+    $resta=$n1-$n2;
+    $suma=$n1+$n2;
+    $multiplicacion=$n1*$n2;
+    $division=$n1/$n2;
+    
+    echo "\n";
+    echo  $resta ;
+    echo "\n";
+
+    echo  $suma;
+    echo "\n";
+
+    echo $multiplicacion;
+    echo "\n";
+
+    echo $division;
+    echo "\n";
+
+?>
