9a2134164df8023caf7b3fb7e83f6406263d345d
Jsr017 <123515664+Jsr017@users.noreply.github.com>
2024-09-24 23:45:36 +0200

    Ejercicio 7,8 variables hecho

--- Tema_1/EjerciciosVariables/variables-1-07.php
+++ Tema_1/EjerciciosVariables/variables-1-07.php
@@ -26,7 +26,25 @@
 
 <?php
 
-print "  <p class=\"aviso\">Ejercicio incompleto</p>\n";
+$color1=rand(0,255);
+$color2=rand(0,255);
+$color3=rand(0,255);
+
+$cuadrado=rand(50,150);
+$cuadrado2=rand(50,150);
+$cuadrado3=rand(50,150);
+
+
+print(
+  '<svg width="750" height="500" viewBox="0 0 1000 1000"  xmlns="http://www.w3.org/2000/svg">
+   <rect x="0" y="0" width="'.$cuadrado.'" height="'.$cuadrado.'" 
+        fill="rgb('.$color2 ." ".$color1 ." ".$color3.')" />
+   <rect x="300" y="0" width="'.$cuadrado2.'" height="'.$cuadrado2.'" 
+        fill="rgb('.$color1 ." ".$color3 ." ".$color2.')" />
+   <rect x="600" y="0" width="'.$cuadrado3.'" height="'.$cuadrado3.'" 
+        fill="rgb('.$color3 ." ".$color2 ." ".$color1.')" />
+  </svg>.'
+);
 
 ?>
 
--- Tema_1/EjerciciosVariables/variables-1-08.php
+++ Tema_1/EjerciciosVariables/variables-1-08.php
@@ -26,7 +26,20 @@
 
 <?php
 
-print "  <p class=\"aviso\">Ejercicio incompleto</p>\n";
+$color1=rand(0,255);
+$color2=rand(0,255);
+$color3=rand(0,255);
+
+$circulo=rand(50,150);
+$circulo2=rand(50,150);
+$circulo3=rand(50,150);
+
+print('
+<svg width="750" height="500" viewBox="0 0 1000 1000"  xmlns="http://www.w3.org/2000/svg">
+<circle   cx="0" cy="200" r="'.$circulo.'" fill="rgb('.$color1 ." ".$color2 ." ".$color2.')"/>
+<circle  cx="300" cy="200" r="'.$circulo2.'" fill="rgb('.$color2 ." ".$color1 ." ".$color3.')"/>
+<circle  cx="600" cy="200" r="'.$circulo3.'" fill="rgb('.$color3 ." ".$color2 ." ".$color1.')"/>
+</svg>.');
 
 ?>
 
